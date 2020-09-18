2d65c04b274658713843bfc384b97910c2398465
Sam Lalli <2sam.lalli@gmail.com>
2020-09-18 10:42:50 -0600

    from created, onchange helper finished, yup installed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Home from "./Home";
 const App = () => {
   const [pizzaOrder, setPizzaOrder] = useState([])
   const [pizzaForm, setPizzaFrom] = useState({
+    name: '',
+    size: '',
+    pepperoni: false,
+    bacon: false,
+    sausage: false,
+    onion: false,
+    ham: false,
+    peppers: false,
+    pineapple: false,
+    mushrooms: false,
+
   })
 
 
@@ -15,7 +26,12 @@ const App = () => {
         <Home />
       </Route>
       <Route path='/pizza'>
-        <Form />
+        <Form 
+        pizzaOrder={pizzaOrder}
+        setPizzaOrder={setPizzaOrder}
+        pizzaForm={pizzaForm}
+        setPizzaFrom={setPizzaFrom}
+        />
       </Route>
     </>
   );
--- src/Form.js
+++ src/Form.js
@@ -2,12 +2,119 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const Form = (props) => {
+const Form = (pizzaOrder, setPizzaOrder, pizzaForm, setPizzaForm) => {
+
+    //onchange
+    const onChange = e => {
+        const {name, value, type, checked} = e.target
+        const trueValue = type === 'checkbox' ? checked : value
+        setPizzaForm({
+            ...pizzaForm, 
+            [name]: trueValue
+        })
+    }
+
+
+    //onsubmit
+
+    //validate
 
 
     return(
         <div>
-            <p>pizza</p>
+            <header>
+            <h1>Lambda Eats</h1>
+            <Link to={'/'}>Home</Link>
+            </header>
+            <h1>Build Your Pizza, Your Way</h1>
+            <h3>fill out the form below to order your pizza</h3>
+            <form>
+                <label>Name:</label>
+                <input 
+                type='text'
+                name='name'
+                //value
+                //onchange
+                placeholder="Enter Name here"
+                />
+                <br></br>
+                <label>Pizza Size</label>
+                <select name="size" //value= onChange={onChange}
+                    >
+                    <option value=''>-select your size-</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='Large'>Large</option>
+                    <option value='Lambda'>Lambda Large</option>
+                </select>
+                <br></br>
+                <label>Pepperoni</label>
+                <input 
+                type='checkbox'
+                name='pepperoni'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Bacon</label>
+                <input 
+                type='checkbox'
+                name='bacon'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Sausage</label>
+                <input 
+                type='checkbox'
+                name='sausage'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Onion</label>
+                <input 
+                type='checkbox'
+                name='onion'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Ham</label>
+                <input 
+                type='checkbox'
+                name='ham'
+                //checked
+                //onchange
+                />
+                <br></br>
+<               label>Peppers</label>
+                <input 
+                type='checkbox'
+                name='peppers'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Pineapple</label>
+                <input 
+                type='checkbox'
+                name='pineapple'
+                //checked
+                //onchange
+                />
+                <br></br>
+                <label>Mushrooms</label>
+                <input 
+                type='checkbox'
+                name='mushrooms'
+                //checked
+                //onchange
+                />
+                <br></br>
+
+                <button type="submit">SUBMIT</button>
+            </form>
         </div>
     )
 }
